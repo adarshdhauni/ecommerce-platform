4f3f2e0d46c165a4fdcf714083f5933145e38fb5
Adarsh Dhauni <adarshdhauni186@gmail.com>
2026-06-24 20:08:56 +0530

    Fix CORS configuration for preview and production builds

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://localhost:4173"],
     credentials: true,
   }),
 );
