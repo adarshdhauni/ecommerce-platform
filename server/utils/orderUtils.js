f66d2706bce99493e2960dff85e39462a9de1865
Adarsh Dhauni <adarshdhauni186@gmail.com>
2026-06-26 19:40:19 +0530

    fix: resolve backend import crash

--- server/utils/orderUtils.js
+++ server/utils/orderUtils.js
@@ -1,4 +1,4 @@
-const Product = require("../models/Product");
+const Product = require("../models/product");
 const updateOrderStatus = require("./updateOrderStatus");
 
 const cancelOrderLogic = async (order) => {
